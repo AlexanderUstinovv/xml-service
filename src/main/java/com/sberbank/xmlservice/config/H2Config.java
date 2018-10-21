3448da7ad17745e16d390010d1b25d2b9e04fe4b
Alexander Ustinov <ustinovaa@rus-dev.com>
2018-10-22 00:27:01 +0500

    Fixed bug with starting database

--- src/main/java/com/sberbank/xmlservice/config/H2Config.java
+++ src/main/java/com/sberbank/xmlservice/config/H2Config.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 import java.sql.SQLException;
 
 @Configuration
 public class H2Config {
+    @PostConstruct
     @Bean(initMethod = "start", destroyMethod = "stop")
     public Server h2Server() throws SQLException {
         return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", port);
