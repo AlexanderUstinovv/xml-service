f670012ef8f525f0ceb4076e65737070131ced72
Alexander Ustinov <ustinovaa@rus-dev.com>
2018-10-21 22:40:00 +0500

    Added port in H2Config class

--- src/main/java/com/sberbank/xmlservice/config/H2Config.java
+++ src/main/java/com/sberbank/xmlservice/config/H2Config.java
@@ -1,6 +1,7 @@
 package com.sberbank.xmlservice.config;
 
 import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,9 @@ import java.sql.SQLException;
 public class H2Config {
     @Bean(initMethod = "start", destroyMethod = "stop")
     public Server h2Server() throws SQLException {
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "8081");
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", port);
     }
+
+    @Value("${spring.h2.port}")
+    private String port;
 }
